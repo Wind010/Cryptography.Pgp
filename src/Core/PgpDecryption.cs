98e8c906973ddb3f970e4971f882ec969958fc3f
Jeff Tong <wind.TWD@gmail.com>
2019-02-15 18:13:47 -0800

    Adding code comments, additional exceptions, and looping the PgpObjectFactory.

--- src/Core/PgpDecryption.cs
+++ src/Core/PgpDecryption.cs
@@ -1,4 +1,5 @@
-﻿using Org.BouncyCastle.Bcpg.OpenPgp;
+﻿
+using Org.BouncyCastle.Bcpg.OpenPgp;
 using Org.BouncyCastle.Utilities.IO;
 
 using System.IO;
@@ -13,9 +14,11 @@ namespace Cryptography.Pgp.Core
     {
         public const string UnableToExtractPrivateKeyFromPgpSecretKeyRingBundle = "Unable to extract private key from PGP secret key ring bundle.";
         public const string EncryptedMessageContainsSignedMessage = "Encrypted message contains a signed message - not literal data.";
-        public const string MessageIsNotASimpleEncryptedFile = "Message is not a simple encrypted file.";
+        public const string MessageIsNotASimpleEncryptedFile = "Message is not a simple encrypted file.";  // Uknown message type encountered.
         public const string FailedToVerifyInputStream = "Failed to verify input stream.";
         public const string FailedToRetrievePgpPublicKeyEncryptedData = "Failed to retrieve PGP public key encrypted data.";
+        public const string CouldNotFindEncryptedDataListInMessage = "Could not find the public key encrypted data list in the PGP message.";
+        public const string SessionKeyNotFound = "Session key to decrypt message was not found.";
 
         public PgpDecryption()
         {
@@ -64,6 +67,12 @@ namespace Cryptography.Pgp.Core
 
             PgpPrivateKey privateKey = null;
 
+            // Now, we have to locate the random session key that is associated with the ciphertext we
+            // need to decrypt. Each encrypted data object in the payload stores the ID of the public/private
+            // key pair that was used to encrypt the data and therefore can decrypt it. We have to search
+            // through the list of encrypted data for the key ID of our key pair in order to figure out
+            // which encrypted items in the list we can decrypt. BouncyCastle will then decrypt the session
+            // key with our private key so that we can decrypt the actual data.
             PgpPublicKeyEncryptedData publicKeyEncryptedData = encryptedDataList.GetEncryptedDataObjects()
                 .Cast<PgpPublicKeyEncryptedData>()
                 .FirstOrDefault(encryptedData => {
@@ -104,6 +113,36 @@ namespace Cryptography.Pgp.Core
                 .Cast<PgpPublicKeyEncryptedData>()
                 .FirstOrDefault(encryptedData =>  encryptedData != null );
 
+            // In addition to the actual data that is stored, PGP *MAY* store information about signatures
+            // in the data stream. The data *MAY* also be compressed before it was encrypted. So a file
+            // may contain nested objects and look like this:
+            //
+            // +---------------------------------------+
+            // | Encrypted payload                     |
+            // | +-----------------------------------+ |
+            // | | Compressed payload                | |
+            // | | +-------------------------------+ | |
+            // | | | One-pass signature list (opt) | | |
+            // | | +-------------------------------+ | |
+            // | | +-------------------------------+ | |
+            // | | | Literal Data (actual data)    | | |
+            // | | +-------------------------------+ | |
+            // | +-----------------------------------+ |
+            // +---------------------------------------+
+            //
+            // If the objects aren't compressed, the view would be much simpler:
+            //
+            // +-----------------------------------+
+            // | Encrypted payload                 |
+            // | +-------------------------------+ |
+            // | | One-pass signature list (opt) | |
+            // | +-------------------------------+ |
+            // | +-------------------------------+ |
+            // | | Literal Data (actual data)    | |
+            // | +-------------------------------+ |
+            // +-----------------------------------+
+
+
             if (publicKeyEncryptedData == null)
             {
                 throw new PgpException(FailedToRetrievePgpPublicKeyEncryptedData);
@@ -114,6 +153,8 @@ namespace Cryptography.Pgp.Core
                 throw new PgpException(FailedToVerifyInputStream);
             }
 
+            // Now that we have the private key, build a stream to read and decrypt data, thus removing the
+            // encryption wrapping all of the other payloads.
             FindAndPipeMessageToStream(publicKeyEncryptedData, keys.Private.Value, outputStream);
         }
 
@@ -124,13 +165,47 @@ namespace Cryptography.Pgp.Core
         {
             PgpObject pgpObject = objFactory?.NextPgpObject();
 
-            // The first object might be a PGP marker packet.
+            // From the OpenPGP standard, RFC 4880 (https://tools.ietf.org/html/rfc4880):
+            // OpenPGP combines symmetric-key encryption and public-key encryption
+            // to provide confidentiality.When made confidential, first the object
+            // is encrypted using a symmetric encryption algorithm. Each symmetric
+            // key is used only once, for a single object. A new "session key" is
+            // generated as a random number for each object (sometimes referred to
+            // as a session). Since it is used only once, the session key is bound
+            // to the message and transmitted with it. To protect the key, it is
+            // encrypted with the receiver's public key.
+            //
+            // To decrypt the data, we have to pull the encrypted random key out of the encrypted payload,
+            // decrypt it with our private key, and then use the random key to decrypt the actual
+            // ciphertext to retrieve the original plaintext. BouncyCastle takes care of a few of these
+            // steps for us like decrypting the session key with our private key.
+            //
+
+            //
+            // Get the list of encrypted data (i.e., the ciphertext and encrypted session key). The first
+            // object in the stream could be either the list of encrypted data or it might be a PGP marker
+            // packet (https://tools.ietf.org/html/rfc4880#section-5.8). If it's just the marker packet,
+            // the encrypted data list should be the next item in the stream.
+
             if (pgpObject is PgpEncryptedDataList)
             {
                 return (PgpEncryptedDataList)pgpObject;
             }
 
-            return (PgpEncryptedDataList)objFactory?.NextPgpObject();
+            // Skip over the marker, and grab the next item. If it isn't the encrypted data, either the
+            // data is corrupt or we don't know how to process it.
+
+            var encryptedDataList = (PgpEncryptedDataList)objFactory?.NextPgpObject();
+            if (null == encryptedDataList)
+            {
+
+                // The documentation on BouncyCastle is non-existent. It only exists through sample code
+                // and StackExchange posts, so if this ever happens, it is entirely possible that we're
+                // encountering a valid stream that we just don't know how to parse. In other words, this
+                // exception does not mean that the stream is bad. It could indicate a bug in our use of
+                // the library.
+                throw new PgpException(CouldNotFindEncryptedDataListInMessage);
+            }
         }
 
         private PgpPrivateKey ExtractPrivateKeyFromSecretKeyRingBundle(PgpSecretKeyRingBundle secretKeyRingBundle, 
@@ -140,7 +215,7 @@ namespace Cryptography.Pgp.Core
 
             if (secretKey == null)
             {
-                return null;
+                throw new PgpException(SessionKeyNotFound);
             }
 
             return secretKey.ExtractPrivateKey(password.ToCharArray());
@@ -152,29 +227,38 @@ namespace Cryptography.Pgp.Core
             using (Stream plainStream = publicKeyEncryptedData.GetDataStream(privateKey))
             {
                 var plainObjectFactory = new PgpObjectFactory(plainStream);
+                PgpObject plainMessage = plainObjectFactory.NextPgpObject();
 
-                PgpObject message = plainObjectFactory.NextPgpObject();
-
-                if (message is PgpOnePassSignatureList)
+                while (plainMessage != null)
                 {
-                    message = plainObjectFactory.NextPgpObject();
-                }
+                    // TODO:  Implement multi-pass signature evaluate case for PgpSignatureList.
+                    if (plainMessage is PgpOnePassSignatureList)
+                    {
+                        plainMessage = plainObjectFactory.NextPgpObject();
+                    }
 
-                if (message is PgpCompressedData)
-                {
-                    PipeToStream(DecompressData(message), outputStream);
-                    return;
-                }
+                    if (plainMessage is PgpCompressedData)
+                    {
+                        // Change the factory over to one that is dealing with compressed data from
+                        // here on out.
+                        PipeToStream(DecompressData(plainMessage), outputStream);
+                        return;
+                    }
 
-                if (message is PgpLiteralData)
-                {
-                    PipeToStream(message, outputStream);
-                    return;
-                }
+                    // If we found literal data, we have found the decrypted data. Stream the literal data to the
+                    // output stream, and get out of here because we're done.
+                    if (plainMessage is PgpLiteralData)
+                    {
+                        PipeToStream(plainMessage, outputStream);
+                        return;
+                    }
 
-                if (message is PgpOnePassSignatureList)
-                {
-                    throw new PgpException(EncryptedMessageContainsSignedMessage);
+                    if (plainMessage is PgpOnePassSignatureList)
+                    {
+                        throw new PgpException(EncryptedMessageContainsSignedMessage);
+                    }
+
+                    plainMessage = plainObjectFactory.NextPgpObject();
                 }
 
                 throw new PgpException(MessageIsNotASimpleEncryptedFile);
@@ -210,6 +294,9 @@ namespace Cryptography.Pgp.Core
 
             Stream uncompressedStream = literalData.GetInputStream();
             Streams.PipeAll(uncompressedStream, outputStream);
+
+            // Reset the output stream so the caller starts reading from the beginning.
+            outputStream.Position = 0;
         }
 
 
